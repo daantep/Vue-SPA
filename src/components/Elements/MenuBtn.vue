02e53e44ff9848cb809d98016f57c0daed53fca0
Daan Teppema <daanthecoder@gmail.com>
2022-05-23 14:53:26 +0200

    adding files

---
+++ src/components/Elements/MenuBtn.vue
@@ -0,0 +1,11 @@
+<script>
+import '../../assets/tailwind.css'
+export default {
+    Components: 'MenuBtn'
+}
+</script>
+<template>
+    <div>
+        <button class="bg-transparent border-orange-600 border-solid text-gray-400 text-3xl">Menu</button>
+    </div>
+</template>
---
+++ src/components/Sections/TitleSection.vue
@@ -0,0 +1,13 @@
+<script>
+import '../../static/tailwind.css'
+export default {
+    Components: 'TitleSection'
+}
+
+</script>
+<template>
+    <section class="bg-bgblack w-full h-96 flex place-content-center" id="TitleSection">
+        <h1 class="text-zinc-50 font-Teko absolute top-36 font-extrabold text-7xl">About Me</h1>
+        <a href="../Menu/"><button class="bg-transparent border-zinc-600 border-4 transition text-white font-btnfont duration-300 ease-linear hover:bg-zinc-600 p-1 border-solid absolute top-2 right-2 text-3xl">Menu</button></a>
+    </section>
+</template>
